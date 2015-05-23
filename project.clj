45cfa2f91b09fe84df52fda430cbb4b2cc0cc0d8
Stanislas Nanchen <stan@hood.ch>
2015-05-23 21:24:32 +0200

    Add project file (for info tracking)
    Signed-off-by: Stanislas Nanchen <stan@hood.ch>

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject elmnt/elmnt-pkgs "0.2.0"
+            :description "PL/SQL utilities"
+            :url "https://github.com/stanislas/elmnt-pkgs"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :eval-in-leiningen true)
