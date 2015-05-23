d974c002acbd2fb2b9df5163e7c7d9d6ff446b79
Stanislas Nanchen <stan@hood.ch>
2015-05-23 18:24:52 +0200

    ELMNT user : grant inherit only from oracle 12c
    Signed-off-by: Stanislas Nanchen <stan@hood.ch>

--- 01_create_elmnt_user.sql
+++ 01_create_elmnt_user.sql
@@ -14,6 +14,22 @@ quota unlimited on users;
 
 -- 2. grant privileges.
 
-grant inherit any privileges to elmnt;
-grant inherit privileges on user sys to elmnt;
-grant select on dba_tablespaces to elmnt;
+declare
+	version_12_and_more number;
+begin
+	select case when exists(select 0
+	                        from v$instance
+	                        where to_number(regexp_substr(version, '\d*')) >= 12)
+		then 1
+	       else 0 end
+	into version_12_and_more
+	from dual;
+	if version_12_and_more = 1
+	then
+		execute immediate 'grant inherit any privileges to elmnt';
+		execute immediate 'grant inherit privileges on user sys to elmnt';
+	end if;
+end;
+/
+
+grant select any dictionary to elmnt;
